1bfbb778642176bd994f41afd9d088946ea39153
Eshwar Muneshar <Emuneshar@gmail.com>
2023-04-18 16:49:16 -0400

    Added code for the snake head

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ window.bgcolor("black")
 window.setup(width=600, height=600)
 window.tracer(0)
 
+# Snake Head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0,0,)
+head.direction = "stop"
 
 
 window.mainloop()
